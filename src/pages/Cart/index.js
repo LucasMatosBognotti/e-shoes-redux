1056234822aab3443d264c7a5f17740247a40c25
Lucas <lucasmatosbognotti@gmail.com>
2020-07-01 20:59:05 -0300

    first commit

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { formatPrice } from '../../util/format';
+import {
+  removeFromCart,
+  updateAmountRequest,
+} from '../../store/modules/cart/actions';
+
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  const dispatch = useDispatch();
+
+  const total = useSelector(state =>
+    formatPrice(
+      state.cart.reduce((totalSum, product) => {
+        return totalSum + product.price * product.amount;
+      }, 0)
+    )
+  );
+
+  const cart = useSelector(state =>
+    state.cart.map(product => ({
+      ...product,
+      subtotal: formatPrice(product.price * product.amount),
+    }))
+  );
+
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map(product => (
+            <tr key={product.id}>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button
+                    type="button"
+                    onClick={() =>
+                      dispatch(
+                        updateAmountRequest(product.id, product.amount - 1)
+                      )
+                    }
+                  >
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+
+                  <input type="text" readOnly value={product.amount} />
+
+                  <button
+                    type="button"
+                    onClick={() =>
+                      dispatch(
+                        updateAmountRequest(product.id, product.amount + 1)
+                      )
+                    }
+                  >
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>{product.subtotal}</strong>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  onClick={() => dispatch(removeFromCart(product.id))}
+                >
+                  <MdDelete size={20} color="#7159c1" />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>{total}</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
